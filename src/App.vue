45fbaa75ceca829397e1e42621f4670ff866e87f
Pinkie Aruwani <aruwanip@gmail.com>
2019-10-09 10:18:32 -0400

    New base code

--- src/App.vue
+++ src/App.vue
@@ -1,22 +1,7 @@
 <template>
-  <v-app>
-    <core-app-bar />
-
-    <core-drawer />
-
-    <core-view />
-
-    <core-footer />
-  </v-app>
+  <router-view></router-view>
 </template>
 
 <script>
-  export default {
-    components: {
-      CoreDrawer: () => import('@/components/core/Drawer'),
-      CoreFooter: () => import('@/components/core/Footer'),
-      CoreAppBar: () => import('@/components/core/AppBar'),
-      CoreView: () => import('@/components/core/View')
-    }
-  }
+export default {};
 </script>
--- src/store/modules/app/state.js
+++
@@ -1,6 +0,0 @@
-export default {
-  drawer: null,
-  color: 'success',
-  image: 'sidebar2.jpg'
-  //https://demos.creative-tim.com/vue-material-dashboard/img/sidebar-2.32103624.jpg'
-}
