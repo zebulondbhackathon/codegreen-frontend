2afa2e6c4298c0e29b966baba4ddddbc4496954d
zebulondbhackathon <zebulon.db.hackathon@outlook.com>
2019-10-10 01:51:37 -0400

    Add filter stuff

--- src/pages/GreenInsights.vue
+++ src/pages/GreenInsights.vue
@@ -3,28 +3,13 @@
     <div class="md-layout">
       <div class="md-layout-item md-medium-size-100 md-xsmall-size-100 md-size-100">
         <md-card>
-          Stock Name:
-          <select>
-            <option>ELECTRONIC ARTS INC</option>
-            <option>FACEBOOK INC-CLASS A</option>
-            <option>NEWS CORP - CLASS B</option>
-            <option>NEWS CORP - CLASS A</option>
-            <option>TEGNA INC</option>
-            <option>FOX CORP - CLASS B</option>
-            <option>ACTIVISION BLIZZARD INC</option>
-            <option>T-MOBILE US INC</option>
-            <option>CBS CORP-CLASS B NON VOTING</option>
-            <option>FRONTIER COMMUNICATIONS CORP</option>
-            <option>FOX CORP - CLASS A</option>
-            <option>OMNICOM GROUP</option>
-            <option>COMCAST CORP-CLASS A</option>
-          </select>
+          <iframe src="/eafilters.html" frameBorder="0" scrolling="no" height="500" width="100%" style="height: 145px"></iframe>
         </md-card>
       </div>
 
       <div class="md-layout-item md-medium-size-100 md-xsmall-size-100 md-size-100">
         <md-card>
-          <iframe src="/eabuysell.html" frameBorder="0" height="500" width="100%" style="height: 500px"></iframe>
+          <iframe src="/eabuysell.html" frameBorder="0" scrolling="no" height="500" width="100%" style="height: 500px"></iframe>
         </md-card>
       </div>
 
